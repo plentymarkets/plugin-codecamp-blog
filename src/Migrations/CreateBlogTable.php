d3213a8a36131728517f3329529381d9738b0a20
David Bohla <david.bohla@plentymarkets.com>
2017-03-20 11:22:56 +0100

    ADD logging in migration

--- src/Migrations/CreateBlogTable.php
+++ src/Migrations/CreateBlogTable.php
@@ -4,11 +4,22 @@ namespace Blog\Migrations;
 
 use Blog\Models\BlogData;
 use Plenty\Modules\Plugin\DataBase\Contracts\Migrate;
+use Plenty\Plugin\Log\Loggable;
 
+/**
+ * Class CreateBlogTable
+ */
 class CreateBlogTable
 {
+    use Loggable;
+
+    /**
+     * @param Migrate $migrate
+     */
     public function run(Migrate $migrate)
     {
         $migrate->createTable(BlogData::class);
+
+        $this->getLogger("CreateBlogTable_run")->debug('Blog::Logging.successMessage', ['tableName' => 'BlogData']);
     }
 }
